434929c83b939b74aa3515a1de41fc3c073ec1c0
TERRY <s1366972@live.hkmu.edu.hk>
2026-03-06 15:52:59 +0800

    heaps and heap_sort prototype

--- task2/heaps.py
+++ task2/heaps.py
@@ -1,65 +1,125 @@
-#  from PrettyPrint import PrettyPrintTree
+from abc import ABC, abstractmethod
+import tree_printer as tprint
+import math
 
 # Heaps & HeapSort
 class Heap:
     def __init__(self, data_array):
         self.data = data_array
-        self.size = len(self.data)
+        self.last_index = len(self.data) - 1
         self.build_heap()
 
     def build_heap(self):
-        for i in range((self.size // 2), 0, -1):
-            self.heapify(i)
+        n = math.ceil(self.last_index / 2) - 1
+        for i in range(n, -1, -1):
+            self.heapify(i, self.last_index)
         return
 
-    def heapify(self, i):
+    @abstractmethod
+    def heapify(self, i, n):
         pass
 
     def get_root(self):
         return self.data[0]
 
-    def get_lchild_index(self, i):
-        return i * 2
+    def left_child(self, i):
+        if i == 0:
+            return 1
+        else:
+            return i * 2 + 1
 
-    def get_rchild_index(self, i):
-        return (i * 2) + 1
+    def right_child(self, i):
+        if i == 0:
+            return 2
+        else:
+            return (i * 2) + 2
 
-    def insert(self):
-        pass
+    def insert(self, item):
+        self.data.append(item)
+        self.last_index += 1
+        self.build_heap()
+        return
 
-    def pop(self):
-        pass
+    def pop(self, i=-1):
+        popped_item = self.data[i]
+        if i == -1:
+            self.data.pop()
+            self.last_index -= 1
+        else:
+            self.data[i] = self.data.pop()
+            self.last_index -= 1
+            self.heapify(i, self.last_index)
+        return popped_item
 
     def extract_root(self):
-        pass
+        root = self.data[0]
+        self.data[0] = self.data[self.last_index]
+        self.data.pop()
+        self.last_index -= 1
+        self.heapify(0, self.last_index)
+        return root
 
-    def heap_sort(self):
-        pass
+    def sort(self):
+        for n in range(self.last_index, -1, -1):
+            self.data[n], self.data[0] = self.data[0], self.data[n]
+            self.heapify(0, n-1)
+        return
 
 class Max_Heap(Heap):
     def __init__(self, data_array):
         super().__init__(data_array)
 
-    def heapify(self, i):
-        l = self.get_lchild_index(i)
-        r = self.get_rchild_index(i)
+    def heapify(self, i, n):
+        l = self.left_child(i)
+        r = self.right_child(i)
         largest = i
-        if l <= self.size and self.data[l-1] > self.data[i-1]:
+        if l <= n and self.data[l] > self.data[i]:
             largest = l
-        else:
-            largest = i
-        if r <= self.size and self.data[r-1] > self.data[largest-1]:
+        if r <= n and self.data[r] > self.data[largest]:
             largest = r
         if largest != i:
-            self.data[i-1], self.data[largest-1] = self.data[largest-1], self.data[i-1]
-            self.heapify(largest)
+            self.data[i], self.data[largest] = self.data[largest], self.data[i]
+            self.heapify(largest, n)
         return
 
 class Min_Heap(Heap):
-    pass
+    def __init__(self, data_array):
+        super().__init__(data_array)
+
+    def heapify(self, i):
+        l = self.left_child(i)
+        r = self.right_child(i)
+        smallest = i
+        if l < self.size and self.data[l] < self.data[i]:
+            smallest = l
+        else:
+            smallest = i
+        if r < self.size and self.data[r] < self.data[smallest]:
+            smallest = r
+        if smallest != i:
+            self.data[i], self.data[smallest] = self.data[smallest], self.data[i]
+            self.heapify(smallest)
+        return
 
 if __name__ == "__main__":
     data_array = [1, 4, 3, 7, 8, 3, 6, 2, 7, 10]
+    print("original data:")
+    print(data_array)
+    print("-" * 50)
+
+    print("max heap:")
     maxheap = Max_Heap(data_array)
     print(maxheap.data)
+    tprint.print_heap(maxheap)
+    print("-" * 50)
 
+    print("pop last item:")
+    maxheap.pop()
+    print(maxheap.data)
+    tprint.print_heap(maxheap)
+    print("-" * 50)
+
+    print("heap sort:")
+    maxheap.sort()
+    print(maxheap.data)
+    tprint.print_heap(maxheap)
