3438c3292c997703e64f4fe4b348bcd64c0c3f16
TERRY <s1366972@live.hkmu.edu.hk>
2026-03-20 15:01:53 +0800

    temp main

--- task1/main.py
+++ task1/main.py
@@ -1,54 +1,37 @@
-from search_strategy import *
-
-class Reader():
-    def __init__(self, id_num, name, borrow_limit=5):
-        self.id_num = id_num
-        self.name = name
-        self.borrow_limit = borrow_limit
-        self.borrowed_book = []
-
-    def borrow_book(self, book):
-        num_of_borrowed = len(self.borrowed_books)
-        if num_of_borrowed < self.borrow_limit:
-            self.borrowed_book.append(book)
-            print("loan succeed")
-        else:
-            print("loan failed")
-
-    def return_book(self, book):
-       pass 
-
-class Book:
-    def __init__(self, title, author, stock=1):
-        self.title = title
-        self.author = author
-        self.stock = max(0, stock)
-
-    def get_title(self):
-        pass
-
-    def get_stock(self):
-        pass
-
-    def reduce_stock(self):
-        pass
-
-    def add_stock(self):
-        pass
-
-class LibraryManager:
-    def __init__(self):
-        self.__books = []
-        self.__readers = []
-
-    def add_book(self, book):
-        self.__books.append(book)
-
-    def add_reader(self, reader):
-        self.__readers.append(reader)
-
-    def search_books(self, keyword, strategy):
-        return strategy.search(self.__books, keyword)
+from library_objects import *
+from database_control import *
 
 if __name__ == "__main__":
-    pass
+    DATABASE_CONN = Database("library.db")
+    READER = Reader(DATABASE_CONN)
+    BOOK = Book(DATABASE_CONN)
+    LIBRARIAN = Librarian(DATABASE_CONN)
+
+    READER.login(10000000)
+    print("reader logged in")
+    print(f"Reader ID: {READER.id_num}, Name: {READER.name}, Borrowed Books: {READER.borrowed_book}")
+
+    print("-" * 50)
+
+    print("borrow book: ")
+    book_id = 20000000
+    if BOOK.is_exist(book_id):
+        BOOK.load_by_id(book_id)
+        if BOOK.stock > 0:
+            READER.borrow_book(book_id)
+            BOOK.reduce_stock()
+            print("borrow successfully")
+    print(f"Title: {BOOK.title}, Author: {BOOK.author}, Stock: {BOOK.stock}")
+    print(f"Reader ID: {READER.id_num}, Name: {READER.name}, Borrowed Books: {READER.borrowed_book}")
+
+    print("-" * 50)
+
+    print("return book: ")
+    book_id = 20000000
+    if BOOK.is_exist(book_id):
+        BOOK.load_by_id(book_id)
+        READER.return_book(book_id)
+        BOOK.add_stock()
+        print("return successfully")
+    print(f"Title: {BOOK.title}, Author: {BOOK.author}, Stock: {BOOK.stock}")
+    print(f"Reader ID: {READER.id_num}, Name: {READER.name}, Borrowed Books: {READER.borrowed_book}")
